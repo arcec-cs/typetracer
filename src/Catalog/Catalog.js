33779e76b71425534a09c1d3a6086e9eb9457406
arcec-cs <arcec.cs@gmail.com>
2021-06-16 15:54:09 -0400

    Created getCurrentDisplay method that conditially renders the TitleCardList, or a BucketList of authors or categories based on the browse state and renders a react-loader-spinner to indicate fetching if data needed to render list is not in localstorage.

--- src/Catalog/Catalog.js
+++ src/Catalog/Catalog.js
@@ -4,13 +4,14 @@ import authors from './temp/authors.json'
 import BucketList from './BucketList';
 import TitleCardList from './TitleCardList';
 import CatalogHeader from './CatalogHeader'
-
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 class App extends Component {
   constructor(props) { 
     super(props);
     
     this.state = { 
-      //browse: 'titles'
+      browse: 'titles'
     }
   }
 
@@ -24,15 +25,34 @@ class App extends Component {
   }  
   componentDidUpdate() {   
   }
+
+  getCurrentDisplay(){
+    //for api calls
+    const loader = <span className='tc'><Loader type="ThreeDots" color="#000000" height={80} width={80} timeout={20000}/></span>;
+    
+    //conditionally render content of the Catalog page based of browse state
+    const browse = this.state.browse; 
+    if(browse === 'titles') 
+      if(localStorage.titleEntries) return <TitleCardList titleInfoArr={JSON.parse(localStorage.titleEntries)}/>;//check localstorage for content
+      else return loader;// if not fetch and indicate loading to user
+    else if(browse == 'authors')
+      if (localStorage.authors) return <BucketList type='author' info={localStorage.authors}/>;
+      else return loader;
+    else if(localStorage.categories) return <BucketList type='category' info={localStorage.categories}/>;
+      else return loader;
+  }
+ 
   
   render() {
     let titleEntries = localStorage.titleEntries; // react throws CORS in chrome when JSON.parse is given null
     if(titleEntries != null) titleEntries = JSON.parse(localStorage.titleEntries)
+    
+    const display = this.getCurrentDisplay(); // conditionally render display acc this.state.browse
+   
     return (
       <div name='CatalogContainer' className=''> 
         <CatalogHeader/>
-        {titleEntries != null && <TitleCardList titleInfoArr={titleEntries}/>}
-        {/* <BucketList type='author' info={authors}/> */}
+        {display}
       </div> 
     );
   }
