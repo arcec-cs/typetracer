6fb534f3bc587ec92a1a8c1d2550d36b42be44d1
arcec-cs <arcec.cs@gmail.com>
2021-06-17 08:46:15 -0400

    Created fetchBrowseByContent to fetch the list selected on the browseByBar. This is an intermediary step for the resource fetching, will need to make this.state.browseBy more broad like this.state.path & make changes in current methods that utilize Browse by next to fetch all and  resources next.

--- src/Catalog/Catalog.js
+++ src/Catalog/Catalog.js
@@ -1,6 +1,6 @@
 import React, {Component } from 'react';
 import titleEntries from './temp/titles.json'
-import authors from './temp/authors.json'
+// import authors from './temp/authors.json'
 import BucketList from './BucketList';
 import TitleCardList from './TitleCardList';
 import CatalogHeader from './CatalogHeader'
@@ -10,20 +10,30 @@ class App extends Component {
   constructor(props) { 
     super(props);
     
+    this.shouldFetch = false;
+    
     this.state = { 
-      browse: 'titles'
+      browseBy: 'titles'
     }
   }
 
   componentDidMount() {
-    // check local storage for titleEntries if user has been on catalog before
-    if(localStorage.getItem('titleEntries') === null){
-      const fetchedTitleInfo = titleEntries; //currently fetching all titles in db
-      localStorage.setItem('titleEntries', JSON.stringify(titleEntries)) // localstorage only store strings
-    }
-    this.setState({}); //to get to re-render for 
+    const browseBy =  this.state.browseBy;
+    if(!localStorage[browseBy])this.fetchBrowseByContent() 
   }  
-  componentDidUpdate() {   
+  componentDidUpdate() { 
+    if(this.shouldFetch === true){ this.fetchBrowseByContent();}
+  }
+
+  fetchBrowseByContent(){
+    const browseBy = this.state.browseBy;
+      fetch(`http://localhost:3005/catalog/${browseBy}`)
+      .then(res=> res.json())
+      .then(data => localStorage[browseBy] = JSON.stringify(data))
+      .then(a => console.log(`Fetching: ${localStorage[browseBy]}`))
+      .then(a => this.shouldFetch = false)
+      .then(a => this.setState({})) //re-render with fetched data
+      .catch(error => console.log(error))
   }
 
   getCurrentDisplay(){
@@ -31,27 +41,24 @@ class App extends Component {
     const loader = <span className='tc'><Loader type="ThreeDots" color="#000000" height={80} width={80} timeout={20000}/></span>;
     
     //conditionally render content of the Catalog page based of browse state
-    const browse = this.state.browse; 
-    if(browse === 'titles') 
-      if(localStorage.titleEntries) return <TitleCardList titleInfoArr={JSON.parse(localStorage.titleEntries)}/>;//check localstorage for content
-      else return loader;// if not fetch and indicate loading to user
-    else if(browse == 'authors')
-      if (localStorage.authors) return <BucketList type='author' info={localStorage.authors}/>;
-      else return loader;
-    else if(localStorage.categories) return <BucketList type='category' info={localStorage.categories}/>;
-      else return loader;
+    const browseBy = this.state.browseBy; 
+    if(browseBy === 'titles') 
+      if(localStorage.titles) return <TitleCardList titleInfoArr={JSON.parse(localStorage.titles)}/>;//check localstorage for content
+      else {this.shouldFetch = true; return loader}// if not fetch and indicate loading to user
+    else if(browseBy == 'authors')
+      if (localStorage.authors) return <BucketList type='author' info={JSON.parse(localStorage.authors)}/>;
+      else {this.shouldFetch = true; return loader}
+    else if(localStorage.categories) return <BucketList type='category' info={JSON.parse(localStorage.categories)}/>;
+      else {this.shouldFetch = true; return loader}
   }
 
   onBrowseByChange = (event) =>{
-    const browseId = event.currentTarget.id
-    this.setState({browse: browseId})
+    const browseBy = event.currentTarget.id
+    this.setState({browseBy: browseBy})
   }
  
   
   render() {
-    let titleEntries = localStorage.titleEntries; // react throws CORS in chrome when JSON.parse is given null
-    if(titleEntries != null) titleEntries = JSON.parse(localStorage.titleEntries)
-    
     const display = this.getCurrentDisplay(); // conditionally render display acc this.
    
     return (
