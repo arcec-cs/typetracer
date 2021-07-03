54eb5f7dae8d0a36bf0785a8da1dddfd1ddaf9ee
arcec-cs <arcec.cs@gmail.com>
2021-07-03 05:29:23 -0400

    fixed sizing bug for 320px diplays by having small browseby bar be w-100.

--- src/Catalog/CatalogHeader.js
+++ src/Catalog/CatalogHeader.js
@@ -10,9 +10,9 @@ const CatalogHeader = ({onBrowseByClick, selected}) => {
     <h1 id='catalogHeader' className='mt2 mb0 baskerville i fw1'>Catalog</h1>
     <h2 className='mt2 mb0 f6 f5-l fw4 ttu tracked'>Browse By:</h2>
     <nav id='browseByBar' className='bt tc center mt3'>
-      <a id='titles' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30 ${isSelected('titl')}`}>{'Texts'}</a>
-      <a id='authors' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30 ${isSelected('author')}`}>{'Authors'}</a>
-      <a id='categories' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30 ${isSelected('categ')}`}>{'Categories'}</a>
+      <a id='titles' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-33 ${isSelected('titl')}`}>{'Texts'}</a>
+      <a id='authors' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-33 ${isSelected('author')}`}>{'Authors'}</a>
+      <a id='categories' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-34 ${isSelected('categ')}`}>{'Categories'}</a>
     </nav>
   </header> 
 
