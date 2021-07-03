f6c4c0821594d7e31211e09c12e75c78c9e8cff1
arcec-cs <arcec.cs@gmail.com>
2021-07-03 04:00:23 -0400

    Modifed BucketList to include Sticky list items that denote the first letter in each bucket entry.

--- src/Catalog/BucketList.js
+++ src/Catalog/BucketList.js
@@ -4,19 +4,35 @@ import React from 'react';
 const BucketList = ({type, info, onItemClick}) => {
   const header = (type === 'author' ? 'Authors:' : 'Categories:');
   const pathFirst = (type === 'author' ? 'authors' : 'categories');
-  const list = info.map(entry =>
-     <li
-     onClick={onItemClick} 
-     id={`${pathFirst}/${entry['id']}`}
-     key={`${entry['id']}`} 
-     className='f3-l ph3 pv3 bb b--light-silver underline pointer'>
-      {entry[type]}
-    </li>);
+  
+  let firstLetterStore;// so we know at beg of letter list
+  const list = info.map(entry => {
+     let stickyBanner;
+     const firstLetter = entry[type].slice(0,1);//get first letter
+     // if fist letter in letter list create sticky banner
+     if(firstLetter != firstLetterStore){
+      firstLetterStore = firstLetter
+      stickyBanner = <li 
+      style={{position:'sticky', top:-1}} 
+      className='f3-l ph2 b b--light-silver pv3 ba bg-near-black white'>
+        {firstLetter}
+      </li>
+     }
+     return <>
+      {stickyBanner} 
+      <li
+      onClick={onItemClick} 
+      id={`${pathFirst}/${entry['id']}`}
+      key={`${entry['id']}`} 
+      className='f3-l ph3 pv3 bb b--light-silver hover-light-red underline pointer'>
+        {entry[type]}
+      </li>
+    </>
+  });
 
   return ( 
-  <article style={{paddingTop: '0'}} className='pa3 pa4-ns'>
-    <h1 className='f3 f2-l bold center mw6 mw7-l mt0'>{header}</h1>
-    <ul className='list pl0 ml0 center mw6 mw7-l ba b--light-silver br2'>
+  <article className='pa4 pl4-l pr0-l w-100 center'>
+    <ul className='list pl0 mt0 center mw6 mw7-l ba b--light-silver br2'>
       {list}
     </ul>
   </article>
