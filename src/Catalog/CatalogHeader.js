15ac51349a508c461274f968a6849f530a5b0b29
arcec-cs <arcec.cs@gmail.com>
2021-07-03 06:11:44 -0400

    Fixed CatalogHeader BrowseBy bar to pointer on hover and change bg color when in bucket because it shall bring them back to the bucktet list. Also fixed typo in TitleCard.css

--- src/Catalog/CatalogHeader.js
+++ src/Catalog/CatalogHeader.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './CatalogHeader.css'
-
 // BucketList component used to make a list of categories or authors, the two "buckets" which Texts go in
 const CatalogHeader = ({onBrowseByClick, selected}) => {
-  const isSelected = (id) => selected.includes(id) ?'underline fw9 i bg-moon-gray white' : 'bg-animate pointer hover-bg-gray hover-white'
-  const selectedText = (id, str) => selected.includes(id) ? `${str}` :  str
+  //browesByBar contional styles
+  const stylesNotSelected = 'underline fw9 i bg-moon-gray white ';
+  const stylesSelected = 'bg-animate pointer hover-bg-gray hover-white ';
+  const stylesSelectedAndInBucket = `${stylesNotSelected} ${stylesSelected}`;
+  //determine what is selected and generate styles
+  const isSelected = (bbText) => selected.includes(bbText);
+  const isInBucket = () => selected.includes('/'); // selected is a path i.e authors/30
+  const style = (bbText) => isSelected(bbText) ? isInBucket() ? stylesSelectedAndInBucket: stylesNotSelected: stylesSelected; 
+  
   return( 
   <header className='pt2 pt3-ns tc black-80 avenir shadow-5'>
     <h1 id='catalogHeader' className='mt2 mb0 baskerville i fw1'>Catalog</h1>
-    <h2 className='mt2 mb0 f6 f5-l fw4 ttu tracked'>Browse By:</h2>
+    <h2 className='mt2 mb0 f6 f5-l fw4 ttu tracked'>{'Browse By:'}</h2>
     <nav id='browseByBar' className='bt tc center mt3'>
-      <a id='titles' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-33 ${isSelected('titl')}`}>{'Texts'}</a>
-      <a id='authors' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-33 ${isSelected('author')}`}>{'Authors'}</a>
-      <a id='categories' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-34 ${isSelected('categ')}`}>{'Categories'}</a>
+      <a id='titles' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-33 ${style('titles')}`}>{'Texts'}</a>
+      <a id='authors' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-33 ${style('authors')}`}>{'Authors'}</a>
+      <a id='categories' onClick ={onBrowseByClick} className={`f5 f4-ns link black-80 dib pa3 ph4-m ph5-l w-30-ns w-34 ${style('categories')}`}>{'Categories'}</a>
     </nav>
   </header> 
-
   );
 }
 export default CatalogHeader;
