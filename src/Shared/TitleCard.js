482d2278263f358c61d13fd2ce502175bc7ca5b7
arcec-cs <arcec.cs@gmail.com>
2021-07-03 05:12:33 -0400

    Revamped TitleCard to have a horizontal instead of vertical format to utilze more of the display on desktop screens. Also made Card more responsive by adding color animations upon hovering on a clickable element, the Tilte of the Card also grows as it is hoverd over since it it the primary element. on -l displays the Title Card is small so we can fit many on the screen, for m/s displays we have it take up 90% of the screen. Medium displays are single column because soon after reaching the breakpoint it goes single col anyways.

--- src/Shared/TitleCard.js
+++ src/Shared/TitleCard.js
@@ -3,46 +3,46 @@ import './TitleCard.css'
 //displays information about a Title and relevant metadata
 const TitleCard = ({title, author, category,  pages, pageProgress,  id, authorId, categoryId, lastAccessed, titleClick, itemClick, type}) => {
   const isTypeX = (bucket) => (type) && type.includes(bucket) ? true : false
-  const clickableStyle = 'underline pointer';
+  const clickableStyle = 'underline pointer hover-light-red';
   //myTexts does not have itemClick
   const getClassName = (bucket) => itemClick ? type && isTypeX(bucket) ? '' : clickableStyle :''; 
-  const classNamePageLi = itemClick && 'tc-ns'
-  const classNamePageUl = itemClick && 'self-center'
   return ( 
-    <article className='center w-custom ba mv4'>
-      <span className='bg-near-black w-100 dib'>
-      <h1
-      id={id}
-      title={title} 
-      className='content-box measure f4 white mv0 pv2 ph4 underline pointer tc'
-      onClick={titleClick}
-      >{title}</h1>
-      </span>
-      <div className='pa2 ph4 bt flex-ns justify-around'>
-        <ul className='list pl0 mt1 mb0 mv1-ns mr1-ns ml4-ns mv2-ns w-34-l w-40-m'>
+    <article id='titleCard' className='flex center mv4'>
+      <div className='flex w-100'>
+        <span name="spacer" className='w-20 dn-nl bg-transparent'></span>
+        <h1
+        id={id}
+        title={title} 
+        className='flex flex-column justify-center h-100 w-100 mv0 pv2 ph3 ph4-l
+        tc f4 white bg-near-black underline pointer grow hover-light-red'
+        onClick={titleClick}
+        >{title}</h1>
+      </div>
+      <div className='w-40 pa2 ph3-ns tl bb bt br'>
+        <ul id='titleInfoList' className='list pl0 mb0 mv2 flex flex-column justify-between'>
           <li className='pv1'>
             <span className={'b'}>{`Author: `}</span>
             <span id={`authors/${authorId}`} className={`${getClassName('author')}`} onClick={itemClick}>
               {author}
             </span>
-            </li>
-          <li className='pv1 pv2-ns'>
+          </li>
+          <li className='pv1'>
             <span className={'b'}>{`Category: `}</span>
             <span id={`categories/${categoryId}`} className={`${getClassName('categ')}`} onClick={itemClick}>
               {category}
             </span> 
           </li>
-        </ul>
-        <ul className={`list pl0 mt0 mb1 mv2-ns ml1-ns mr4-ns w-34-l w-40-m ${classNamePageUl}`}> 
-          <li className={`pv1 ${classNamePageLi}`}>
-            { (itemClick) ?   <span className='tc'><span className={'b'}>{`Pages:`}</span> {`${pages}`}</span>
-              : <span><span className={'b'}>{`Pages Completed:`}</span> {`${pageProgress}/${pages}`}</span>
+          <li className={`pv1`}>
+            { (itemClick) ? <span className=''><span className={'b'}>{`Pages:`}</span> {`${pages}`}</span>
+              : <span><span className={'b'}>{`Pages Completed:`}</span>{`${pageProgress}/${pages}`}</span>
             }
           </li>
-          {lastAccessed && <li className='pv1 pv2-ns'>
-            <span className={'b'}>{`Last Accessed: `}</span>
-            {`${new Date(lastAccessed * 1000).toString().slice(0, 21)}`}
-          </li>}
+          { (lastAccessed) && 
+            <li className='pv1'>
+              <span className={'b'}>{`Last Accessed: `}</span>
+              {`${new Date(lastAccessed * 1000).toString().slice(0, 21)}`}
+            </li>
+          }
         </ul>
       </div>
     </article>
