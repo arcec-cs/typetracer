95260df2101a561fd00c9b22f335d396eb91567c
arcec-cs <arcec.cs@gmail.com>
2021-07-16 21:12:53 -0400

    Implemented setState and useEffect Hooks in TitleCardList.js  to to fix lag on catalog selection.  State titleInfoArrRec get titles recursivly added to 5 at a time unitl it has reached the full titleInfoArr size. this allow the cards the cards at the begining of the list to appear instanly so ther is no studder on catalog selection. we wrap updating the state in a setTimout so the user can still interact with the cards.

--- src/Shared/TitleCardList.js
+++ src/Shared/TitleCardList.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import TitleCard from './TitleCard';
 const TitleCardList = ({titleInfoArr, type, onTitleClick, onItemClick}) => { 
-  console.log(titleInfoArr)
+  //state titleInfoArrRec to hold previous cards for each render 
+  const[titleInfoArrRec, setTitleInfoArrRec] = useState(titleInfoArr.slice(0,5));
+  //recursively set state to render cards 5 at a time for titleInfoArr is large and causes studder if rendered all at once
+  useEffect(() => { 
+    (function recursive () {
+      setTimeout(() => {
+        let hasMore = titleInfoArrRec.length + 5 < titleInfoArr.length;
+        setTitleInfoArrRec((prevTitleInfoArrRec) => titleInfoArr.slice(0, prevTitleInfoArrRec.length + 5));
+        if (hasMore) recursive();
+      }, 0);//set timeout so users can still interact with UI
+    })()
+  }, []);// [] never changes so only runs on mount
+
   return ( 
-    titleInfoArr.map(titleInfo => 
+    titleInfoArrRec.map(titleInfo => 
       <TitleCard 
         key={titleInfo.id}
         id={titleInfo.id}
